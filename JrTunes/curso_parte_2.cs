905fca99be912c667b37e576bed329465b61c73c
Jair <jairmsn@hotmail.com>
2021-05-08 16:06:33 -0300

    Organizando projeto para inciar a parte dois do curso

---
+++ JrTunes/curso_parte_2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JrTunes
+{
+    class curso_parte_2
+    {
+
+
+
+    }
+}
